5489d9c4475c26da9f70adb4c3e93ec1d9e7af58
Atikulla2001 <atikullahasib2001@gmail.com>
2025-01-30 13:03:18 +0600

    java script07 file updated

---
+++ script.js
@@ -0,0 +1,90 @@
+
+// ------ Math. round ----- //
+
+// let test = 1.9;
+// let test = 1.49;
+
+// ------ Math. round ----- //
+
+// ------ Math. ceil  ----- //
+
+// let test = 1.9;
+// let test = 1.1;
+
+// ------ Math. ceil  ----- //
+
+// ------ Math. floor ----- //
+
+// let test = 1.9;
+// let test = 1.49;
+
+// ------ Math. floor ----- //
+
+// ------ Math. trunc ----- //
+
+// let test = 1.9;
+// let test = 1.49;
+
+// ------ Math. trunc ----- //
+
+
+// console.log(Math.trunc(test));
+
+
+// ======== Math. random ======= //
+
+// let test = Math.random();
+
+// let test = Math.random();
+
+// let test = Math.random() *10;
+
+// let test = Math.random(). toFixed(1);
+
+// let test = Math.random()* 10 ;
+
+
+// console.log(Math.round(test));
+
+// ======== date method ======= //
+
+// let test = new Date();
+
+// let test = new Date();
+
+// console.log(test.getDate());
+
+// console.log(test.getDay() + 2);
+
+// console.log(test.getFullYear());
+
+// console.log(test.getHours());
+
+// console.log(test.getMilliseconds());
+
+// console.log(test.getMonth());
+
+// console.log(test.getSeconds());
+
+// console.log(test.getUTCDate());
+
+
+30 / 1 / 2025
+
+// let test = new Date();
+
+// console.log(`${test.getDate()} / ${test.getMonth() + 1} / ${test.getFullYear()}`);
+
+// console.log(`${test.getHours()} : ${test.getMinutes()} : ${test.getSeconds()}`);
+
+// let test = new Date();
+
+// test.setFullYear(2060)
+
+// console.log(test);
+
+
+// ======== date method ======= //
+
+
+
